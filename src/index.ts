f7e378a59767071bb4ae4cc28c49a216bf9f4aa0
Alla Kokhaniuk <alla.kohanjuk@gmail.com>
2026-06-08 15:53:02 +0300

    init worker bot

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { fromHono } from "chanfana";
+import { Env, Hono } from "hono";
+import { TaskCreate } from "./endpoints/taskCreate";
+import { TaskDelete } from "./endpoints/taskDelete";
+import { TaskFetch } from "./endpoints/taskFetch";
+import { TaskList } from "./endpoints/taskList";
+
+// Start a Hono app
+const app = new Hono<{ Bindings: Env }>();
+
+// Setup OpenAPI registry
+const openapi = fromHono(app, {
+  docs_url: "/",
+});
+
+// Register OpenAPI endpoints
+openapi.get("/api/tasks", TaskList);
+openapi.post("/api/tasks", TaskCreate);
+openapi.get("/api/tasks/:taskSlug", TaskFetch);
+openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+
+// You may also register routes for non OpenAPI directly on Hono
+// app.get('/test', (c) => c.text('Hono!'))
+
+// Export the Hono app
+
+app.post("/telegram/webhook", async (c) => {
+  const update = await c.req.json();
+
+  console.log(update);
+
+  return c.text("ok");
+});
+
+export default app;
